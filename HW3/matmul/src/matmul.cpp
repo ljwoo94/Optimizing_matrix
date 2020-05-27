6ebb798639bc9c382cd225436eccc3b39f8b3890
NerdyLee <ljwoo94@gmail.com>
2020-05-27 16:40:49 +0900

    block matrix single

--- HW3/matmul/src/matmul.cpp
+++ HW3/matmul/src/matmul.cpp
@@ -1,4 +1,5 @@
 #include "matmul.h"
+#include <thread>
 
 void matmul_ref(const int* const matrixA, const int* const matrixB,
                 int* const matrixC, const int n) {
@@ -13,10 +14,25 @@ void matmul_optimized(const int* const matrixA, const int* const matrixB,
                       int* const matrixC, const int n) {
   // TODO: Implement your code
 
-  //
-  //apply Transpose by changing order. (better cache line)
-  for (int i = 0; i < n; i++)
-    for (int k = 0; k < n; k++)
-      for (int j = 0; j < n; j++)
-        matrixC[i * n + j] += matrixA[i * n + k] * matrixB[k * n + j];
+  //Blocked Matrix Multiply L1 cache = 32K
+  //int = 4byte. 32K = 32,768 B
+  //L1d can hold 8192 ints.
+  //cache line = 64 bit = 8 B => 2 consecutive ints
+  //about 52x52 matrix = 1 L1cache without capacity miss
+  int b = 52;
+  for(int i = b; i > 0; i++) {
+    if(n%i == 0) {
+      b = i;
+      break;
+    }
+  }
+
+   for(int i=0; i<n; i+=b)
+        for(int j=0; j<n; j+=b)
+            for(int k=0; k<n; k+=b)
+                for(int ii=0; ii<b; ii++)
+                    for(int jj=0; jj<b; jj++)
+                        for(int kk=0; kk<b; kk++)
+                            matrixC[(i+ii)*n + j+jj] += matrixA[(i+ii)*n + k+kk]*matrixB[(k+kk)*n + j+jj];
+    
 }
