374263fae80864c413363e78595ec2c982ef5eff
NerdyLee <ljwoo94@gmail.com>
2020-05-27 16:57:18 +0900

    block_trans_single

--- HW3/matmul/src/matmul.cpp
+++ HW3/matmul/src/matmul.cpp
@@ -13,10 +13,20 @@ void matmul_optimized(const int* const matrixA, const int* const matrixB,
                       int* const matrixC, const int n) {
   // TODO: Implement your code
 
-  //
-  //apply Transpose by changing order. (better cache line)
-  for (int i = 0; i < n; i++)
-    for (int k = 0; k < n; k++)
-      for (int j = 0; j < n; j++)
-        matrixC[i * n + j] += matrixA[i * n + k] * matrixB[k * n + j];
+  int b = 52;
+  for(int i = b; i > 0; i++) {
+    if(n%i == 0) {
+      b = i;
+      break;
+    }
+  }
+
+  //block + trans single thread
+  for(int i=0; i<n; i+=b)
+    for(int k=0; k<n; k+=b)
+      for(int j=0; j<n; j+=b)   
+        for(int ii=0; ii<b; ii++)
+          for(int kk=0; kk<b; kk++)
+            for(int jj=0; jj<b; jj++)
+              matrixC[(i+ii)*n + j+jj] += matrixA[(i+ii)*n + k+kk]*matrixB[(k+kk)*n + j+jj];
 }
