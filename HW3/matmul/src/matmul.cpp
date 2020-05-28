4b2478fa72061c32d2ae44b9c576481a118bb0a9
NerdyLee <ljwoo94@gmail.com>
2020-05-28 11:28:40 +0900

    multi block should be optimized

--- HW3/matmul/src/matmul.cpp
+++ HW3/matmul/src/matmul.cpp
@@ -1,5 +1,5 @@
 #include "matmul.h"
-
+#include <omp.h>
 void matmul_ref(const int* const matrixA, const int* const matrixB,
                 int* const matrixC, const int n) {
   // You can assume matrixC is initialized with zero
@@ -13,21 +13,25 @@ void matmul_optimized(const int* const matrixA, const int* const matrixB,
                       int* const matrixC, const int n) {
   // TODO: Implement your code
 
-  int b = 52;
+  int b = 32;
   for(int i = b; i > 0; i++) {
     if(n%i == 0) {
       b = i;
       break;
     }
   }
-
+  omp_set_num_threads(omp_get_num_procs());
   //block + trans multi-thread
-  #pragma omp parallel for collapse(6)
+  #pragma omp parallel for
   for(int i=0; i<n; i+=b)
     for(int k=0; k<n; k+=b)
-      for(int j=0; j<n; j+=b)   
+     // #pragma omp parallel for 
+      for(int j=0; j<n; j+=b)
+    //  #pragma omp parallel for   
         for(int ii=0; ii<b; ii++)
-          for(int kk=0; kk<b; kk++)
-            for(int jj=0; jj<b; jj++)
-              matrixC[(i+ii)*n + j+jj] += matrixA[(i+ii)*n + k+kk]*matrixB[(k+kk)*n + j+jj];
+          for(int kk=0; kk<b; kk++) {
+            int matA = matrixA[(i+ii)*n+ k+kk];
+             for(int jj=0; jj<b; jj++)
+              matrixC[(i+ii)*n + j+jj] += matA*matrixB[(k+kk)*n + j+jj];
+          }    
 }
